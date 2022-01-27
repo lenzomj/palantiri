dedab69e0e0ecc5a5a66aa1cc35fe053080dd101
Matthew J. Lenzo <matthew.lenzo@gmail.com>
2022-01-26 22:54:50 -0500

    Cleanup server code

--- server/index.js
+++ server/index.js
@@ -1,46 +1,35 @@
 // server/index.js
 
-// import the express and express-ws libraries
-const express = require('express')
-const expressWs = require('express-ws')
+/*
+ * Enable express style routes for WebSocket endpoints.
+ */
+const express = require('express');
+const app = express();
+const expressWs = require('express-ws')(app);
 
-// create a new express application
-const app = express()
-// decorate the app instance with express-ws to have it
-// implement websockets
-expressWs(app)
+const WSPORT = 8080;
+const clients = new Set();
 
-// Create a new set to hold each clients socket connection
-const connections = new Set()
-
-// We define a handler that will be called everytime a new
-// Websocket connection is made
+/*
+ * Define a WebSocket handler and add it to the /chat route.
+ */
 const wsHandler = (ws) => {
-  // Add the connection to our set
-  connections.add(ws)
+  clients.add(ws);
 
-  // We define the handler to be called everytime this
-  // connection receives a new message from the client
   ws.on('message', (message) => {
-    // Once we receive a message, we send it to all clients
-    // in the connection set
-    connections.forEach((conn) => conn.send(message))
-  })
+     clients.forEach((client) => client.send(message));
+  });
 
-  // Once the client disconnects, the `close` handler is called
   ws.on('close', () => {
-    // The closed connection is removed from the set
-    connections.delete(ws)
-  })
-}
+    clients.delete(ws);
+  });
+};
 
-// add our websocket handler to the '/chat' route
-app.ws('/chat', wsHandler)
+app.ws('/chat', wsHandler);
 
-// host the static files in the build directory
-// (we will be using this later)
-app.use(express.static('build'))
-
-// start the server, listening to port 8080
-app.listen(8080)
-console.log("Listening on port 8080");
+/*
+ * Start the server.
+ */
+app.use(express.static('build'));
+app.listen(WSPORT);
+console.log(`Listening on port ${WSPORT}`);
